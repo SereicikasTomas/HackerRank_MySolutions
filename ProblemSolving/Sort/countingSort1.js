a0c21aa47a0b865a265f3cfb3e6aeb1bd79a7fd9
SereicikasTomas <sereicikas.t@gmail.com>
2018-11-03 17:38:45 +0100

    commentary for countingSort1

--- ProblemSolving/Sort/countingSort1.js
+++ ProblemSolving/Sort/countingSort1.js
@@ -1,14 +1,15 @@
 function countingSort(arr) {
+    // Create array to store values
     let a = [];
+    // Loop through numbers from 0 to 99
     for ( let i = 0; i <= 99; i++){
+        // Set counter for repeating numbers
         let count = 0;
-        a[i] = 0;
-        for ( let j = 0; j <= arr.length; j++){
-            if (i == arr[j]){
-                count ++;
-                a[i] = count;
-            }
-        }
+        // Loop array and compare numbers with numbers from array
+        arr.forEach(num => {
+            i == num && count ++;
+                        a[i] = count;
+        });
     }
     return a;
 }
--- ProblemSolving/Strings/camelCase.js
+++ ProblemSolving/Strings/camelCase.js
@@ -1,5 +1,5 @@
 function camelcase(s) {
-    // Using RegEx find words that begin with uppercase letter and split the to different words in array.
+    // Using RegEx find words that begin with uppercase letter and split them to different words in array.
     let arr = s.split(/[A-Z]/);
     // Return how many words are in array.
     return arr.length;
