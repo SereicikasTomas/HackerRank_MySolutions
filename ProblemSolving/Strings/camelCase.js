ebf0356a08a55a419706a500e5e8e25f5832656c
SereicikasTomas <sereicikas.t@gmail.com>
2018-11-03 17:02:22 +0100

    commentary for camelCase

--- ProblemSolving/Strings/camelCase.js
+++ ProblemSolving/Strings/camelCase.js
@@ -1,4 +1,6 @@
 function camelcase(s) {
-    let arr = s.split(/(?=[A-Z])/);
+    // Using RegEx find words that begin with uppercase letter and split the to different words in array.
+    let arr = s.split(/[A-Z]/);
+    // Return how many words are in array.
     return arr.length;
 }
