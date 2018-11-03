6a6b300ede1b43a9bcbfb0107b03522c4cd39955
SereicikasTomas <sereicikas.t@gmail.com>
2018-11-03 16:35:44 +0100

    gradingStudents comments and shorter version

--- ProblemSolving/Implementation/gradingStudents.js
+++ ProblemSolving/Implementation/gradingStudents.js
@@ -1,6 +1,9 @@
 function gradingStudents(grades) {
+    // Create an empty array where you will store rounded grades
     let grad = [];
+    // Loop through all grades 
     for(let i = 0; i < grades.length; i++){
+        // Check how much the grade is missing to be rounded and add that number.
         if ( grades[i]%5 == 3 && grades[i] >=38 ){
             grad.push(grades[i]+2);
         } else if ( grades[i]%5 == 4 && grades[i] >=38 ) {
@@ -9,5 +12,16 @@ function gradingStudents(grades) {
             grad.push(grades[i]);
         }
     }
+    return grad;
+}
+
+// Shorter version
+
+function gradingStudents(grades) {
+    let grad = [];
+    grades.forEach(grade => { 
+        grade%5 == 3 && grade >=38 ? grad.push(grade+2):
+        grade%5 == 4 && grade >=38 ? grad.push(grade+1): grad.push(grade);
+    });
     return grad;
 }
